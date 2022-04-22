d3683a739b633b7842d273754bf4206131eb706f
Kirill <kirilldyachkovskiy@gmail.com>
2022-04-22 20:43:58 +0300

    add preprocessor

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.scss';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
